9e0ef00bf89d9f5d7835ab7f5595c7a634631dff
Fellipe Medeiros <fellipe.medeiros@hotmail.com>
2020-10-26 23:10:26 -0300

    added now route

--- server.js
+++ server.js
@@ -6,6 +6,18 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/now', (req, res) => {
+   let ts = Date.now();
+
+   let date_ob = new Date(ts);
+   let date = date_ob.getDate();
+   let month = date_ob.getMonth() + 1;
+   let year = date_ob.getFullYear();
+
+   
+   res.send(year + "-" + month + "-" + date);
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })

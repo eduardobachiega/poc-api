5a2a2f4fa6be0be5f9d6ee220975e70f3fe0e742
Fellipe Medeiros <fellipe.medeiros@hotmail.com>
2020-10-26 23:30:53 -0300

    added stub route to test conectivity

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const https = require('https')
 const app = express()
 const port = 3000
 
@@ -16,6 +17,38 @@ app.get('/now', (req, res) => {
 
    
    res.send(year + "-" + month + "-" + date);
+})
+
+app.get('/html', (req, res) => {
+   res.send("<p> HTML TEST!!!</p>")
+})
+
+app.get('/dog', (req, res) => {
+   const options = {
+      'method': 'GET',
+      'hostname': 'dog.ceo',
+      'path': '/api/breeds/image/random'
+    };
+
+    var requisition = https.request(options, function (response) {
+      var chunks = [];
+    
+      response.on("data", function (chunk) {
+        chunks.push(chunk);
+      });
+    
+      response.on("end", function (chunk) {
+         var body = Buffer.concat(chunks);
+         res.send(body.toString())
+         console.log(body.toString());
+      });
+    
+      response.on("error", function (error) {
+        console.error(error);
+      });
+    });
+    
+    requisition.end();
 })
 
 app.listen(port, () => {
